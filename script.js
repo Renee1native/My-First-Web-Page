e4410243659d0500a4686e118ee3891846f3efb3
Renee Herrera <rene1native@gmail.com>
2026-08-31 04:35:00 -0700

    Add dynamic skill list functionality: implement add and remove item features

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ styleTextBox.addEventListener("input", () => {
     indexForClass = (indexForClass + 1) % 3; // Modulo
     styleTextBox.classList.add(myClassList[indexForClass]);
 })
+
+
+//-----------------------------------------------------------------------//
+const skillInput = document.querySelector("#item-input");
+const addSkillButton = document.querySelector("#add-item-button");
+const skillList = document.querySelector("#item-list");
+
+addSkillButton.addEventListener("click", () => {
+    const newSkillValue = skillInput.value;
+
+    const newSkillItem = document.createElement("li");
+    newSkillItem.textContent = newSkillValue;
+
+    skillList.appendChild(newSkillItem);
+    skillInput.value = "";
+
+})
+
+const removeSkillButton = document.querySelector("#remove-item-button");
+removeSkillButton.addEventListener("click", () => {
+    const fullSkillList = document.querySelectorAll("#item-list li");
+    fullSkillList[fullSkillList.length - 1].remove();
+})
